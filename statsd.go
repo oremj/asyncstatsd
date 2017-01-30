cdcbacaaa764013a6be319e7ad1fc47145861a7d
Jeremiah Orem <oremj@oremj.com>
2017-01-30 15:13:39 -0800

    add noop client

--- statsd.go
+++ statsd.go
@@ -6,75 +6,75 @@ import (
 	"github.com/alexcesaro/statsd"
 )
 
-type Client struct {
+type statsdclient struct {
 	statsd *statsd.Client
 
 	queue *RunQueue
 }
 
-// New returns a new client
-func New(c *statsd.Client, queueSize int) *Client {
-	return &Client{
+// New returns a new statsdclient
+func New(c *statsd.Client, queueSize int) Client {
+	return &statsdclient{
 		statsd: c,
 		queue:  NewRunQueue(queueSize),
 	}
 }
 
-func (c *Client) Count(bucket string, n interface{}) {
+func (c *statsdclient) Count(bucket string, n interface{}) {
 	c.queue.Queue(func() {
 		c.statsd.Count(bucket, n)
 	})
 }
 
-func (c *Client) Gauge(bucket string, value interface{}) {
+func (c *statsdclient) Gauge(bucket string, value interface{}) {
 	c.queue.Queue(func() {
 		c.statsd.Gauge(bucket, value)
 	})
 }
 
-func (c *Client) Increment(bucket string) {
+func (c *statsdclient) Increment(bucket string) {
 	c.queue.Queue(func() {
 		c.statsd.Increment(bucket)
 	})
 }
 
-func (c *Client) Histogram(bucket string, value interface{}) {
+func (c *statsdclient) Histogram(bucket string, value interface{}) {
 	c.queue.Queue(func() {
 		c.statsd.Histogram(bucket, value)
 	})
 }
 
-func (c *Client) Timing(bucket string, value interface{}) {
+func (c *statsdclient) Timing(bucket string, value interface{}) {
 	c.queue.Queue(func() {
 		c.statsd.Timing(bucket, value)
 	})
 }
 
-func (c *Client) Clone(opts ...statsd.Option) *Client {
+func (c *statsdclient) Clone(opts ...statsd.Option) Client {
 	c.statsd.Clone()
-	return &Client{
+	return &statsdclient{
 		statsd: c.statsd.Clone(opts...),
 		queue:  c.queue,
 	}
 }
 
-type Timing struct {
+type statsdtiming struct {
 	timing statsd.Timing
-	c      *Client
+	c      Client
 }
 
-// NewTiming returns a new wrapped timing struct
-func (c *Client) NewTiming() Timing {
-	return Timing{
+// Newstatsdtiming returns a new wrapped timing struct
+func (c *statsdclient) NewTiming() Timing {
+	return statsdtiming{
 		timing: c.statsd.NewTiming(),
 		c:      c,
 	}
 }
 
-func (t Timing) Send(bucket string) {
+func (t statsdtiming) Send(bucket string) {
 	t.c.Timing(bucket, int(t.Duration()/time.Millisecond))
 }
 
-func (t Timing) Duration() time.Duration {
+func (t statsdtiming) Duration() time.Duration {
 	return t.timing.Duration()
 }
